f96de0da22e76daa59f3868e46a24b26e6d64dbc
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2020-02-02 05:31:05 +0100

    Update install.sh

--- includes/install.sh
+++ includes/install.sh
@@ -2,22 +2,23 @@
 
 # INSTALL BeEF
 
-apt-get -y install sqlite3 libsqlite3-dev
-apt-get -y install build-essential ruby ruby-dev zlib1g-dev libruby libssl-dev libpcre3-dev libcurl4-openssl-dev rake ruby-rack rails
+#apt-get -y install sqlite3 libsqlite3-dev
+#apt-get -y install build-essential ruby ruby-dev zlib1g-dev libruby libssl-dev libpcre3-dev libcurl4-openssl-dev rake ruby-rack rails
 
 #gawk, g++, make, libreadline6-dev, zlib1g-dev, libssl-dev, libyaml-dev, autoconf, libgdbm-dev, libncurses5-dev, automake, libtool, bison, pkg-config, libffi-dev
 
-wget https://github.com/xtr4nge/beef/archive/master.zip -O beef-master.zip
+#wget https://github.com/xtr4nge/beef/archive/master.zip -O beef-master.zip
+wget https://github.com/beefproject/beef/archive/master.zip -O beef-master.zip
 
 unzip beef-master.zip
-
 cd beef-master
+./install
 
-echo
-echo "installing dependencies..."
-gem install bundler
+#echo
+#echo "installing dependencies..."
+#gem install bundler
 
-bundle install
+#bundle install
 
 cd ../
 echo
