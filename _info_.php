2d26f99d2ba3197a9cc4d0c53dd0e4cb0e40f586
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2020-02-10 16:20:48 +0100

    Update _info_.php

--- _info_.php
+++ _info_.php
@@ -1,39 +1,61 @@
 <?
+$mod_type="service";
 $mod_name="beef";
-$mod_version="1.3";
+$mod_alias="Beef";
+$mod_version="2.b";
+$mod_dep="beef";
+$mod_co="Mitmproxy";
+$mod_cotwo="Msfrpcd";
+$mod_panel="show";
+$mod_isup="ps aux | grep -iEe 'beef' | grep -v grep";
+$mod_coisup="ps aux|grep -E 'mitmdump.+inject_beef' | grep -v grep";
+$mod_cotwoisup="ps aux|grep -E 'msfrpcd' | grep -v grep";
+
+$mod_logs_panel="enabled";
 $mod_path="/usr/share/fruitywifi/www/modules/$mod_name";
 $mod_logs="$log_path/$mod_name.log"; 
 $mod_logs_history="$mod_path/includes/logs/";
-$mod_logs_panel="enabled";
-$mod_panel="show";
-$mod_type="module";
-$mod_alias="BeEF";
-$mod_isup="ps aux | grep -iEe 'ruby.+beef' | grep -v grep";
-$mod_msfbacktask="ps aux | grep -iEe 'msfrpcd' | grep -v grep"; //msfrpcd check
+
+$mod_optpath="$mod_path/includes/beef/config.yaml";
+$mod_cooptpath="$mod_path/includes/beef/extension/metasploit/config.yaml";
 
 # OPTIONS
 $mod_beef_kali="0";
 $mod_beef_auto="0";
 
-#User Options
-$mod_beef_conf1="$mod_path/config.yaml";
-$mod_beef_conf2="$mod_path/extensions/metasploit/config.yaml";
-$mod_beef_msfenable="0";
-$mod_beef_msfhost="127.0.0.1";
-$mod_beef_msfport="55553";
-$mod_beef_msfuser="msf";
-$mod_beef_msfpass="abc123";
-$mod_beef_msfsslenable="0";
-$mod_beef_msfssltype="TLS1";
-$mod_beef_msfsslverify="0";
-$mod_beef_msfautopwnurl="autopwn";
-$mod_beef_msfcallbackhost="127.0.0.1";
+# USER OPTIONS
+# BEEF
+$mod_user = "beef";
+$mod_passwd = "beef";
+$mod_xhr_poll_timeout = "5000";
+$mod_hook_file = "hook.js";
+$mod_hook_session_name = "BEEF";
+$mod_dns_hostname_lookup = "true";
+$mod_sslbeef = "false";
+$mod_sslcertpath = "";
+$mod_sslkeypath = "";
+$mod_metasploit = "false";
+
+#MSFRPC
+$mod_msfhost = "127.0.0.1";
+$mod_msfport = "55553";
+$mod_msfuser = "msf";
+$mod_msfpasswd = "Hackme01!";
+
+$mod_msfsslenable = "false";
+$mod_msfsslversion = "TLS1";
+$mod_msfsslverify = "false";
+$mod_msfcallback_host =  "10.0.0.1";
+$mod_msfautopwn_url =  "autopwn";
 
 # EXEC
 $bin_sudo = "/usr/bin/sudo";
-$bin_beef = "$mod_path/includes/beef";
-$bin_mitmproxy = "/usr/local/bin/mitmdump";
+$bin_beef = "$mod_path/includes/beef/beef";
+$bin_msfrpcd= "/usr/bin/msfrpcd";
+$bin_mitmdump = "/bin/mitmdump";
 $bin_iptables = "/sbin/iptables";
+
+
 $bin_ifconfig = "/sbin/ifconfig";
 $bin_iwlist = "/sbin/iwlist";
 $bin_sh = "/bin/sh";
